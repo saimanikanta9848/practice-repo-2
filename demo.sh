c5b3cae83bd6afe55a6970e112b87e1ee8100191
saimanikanta9848 <ayyappa745@gmail.com>
2022-05-17 12:56:33 +0530

    create bash file

---
+++ demo.sh
@@ -0,0 +1,14 @@
+
+#! /bin/bash
+# this is a comment
+
+name=Manikanta
+VALUE=10
+education=btech
+height=5.7
+
+echo my name is $name
+echo value $VALUE
+echo qualification $education
+echo length $height
+
